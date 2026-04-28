f1137a3bb3f89cda218ab45f8326e83bb67f9bd5
연구개발전담부 <wdpb@mig.kr>
2026-04-28 16:39:55 +0900

    fix

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["/api/", "/inquiry/"],
     },
     sitemap: "https://sunoneroom.com/sitemap.xml",
   };
